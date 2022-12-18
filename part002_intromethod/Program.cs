b161a88d029836908327ceee938ad331c11cc494
Pavel Grigorev <pavel.grigorev.1990@mail.ru>
2022-12-18 17:50:26 +0300

    added part002_intromethod

---
+++ part002_intromethod/Program.cs
@@ -0,0 +1,20 @@
+﻿//Функция Max:
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = {12, 21, 32, 14, 51, 16, 7, 18, 9}; //создание массива
+
+array[0] = 12;
+Console.WriteLine(array[0]);
+
+int result = Max (
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+Console.WriteLine(result);
